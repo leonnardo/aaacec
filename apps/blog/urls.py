7c4c4b7f19c8988f267037b90e8675b809e27d7c
Leonnardo Rabello <leonnardo@gmail.com>
2012-05-22 23:56:17 -0300

    Creating models + urls config

---
+++ apps/blog/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls.defaults import *
+from aaacec.apps.blog.models import Post
+urlpatterns = patterns("",
+    url(r"^$", "django.views.generic.date_based.archive.index",{'queryset': Post.objects.all(), 'date_field': 'pub'}),
+)
