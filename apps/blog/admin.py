ed639083f9de90908384562a900720b4cfe911ff
Leonnardo Rabello <leonnardo@gmail.com>
2012-05-25 02:56:15 -0300

    Some more modifications

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,12 +1,31 @@
+# -*- coding: UTF-8 -*-
 from django.contrib import admin
 from models import Post
 
 class PostAdmin(admin.ModelAdmin):
     exclude = ('author',)
     list_display = ('title', 'pub_date', 'author')
+    search_fields = ['title', 'author']
+    
+    # Set the post's author based on the logged user
     def save_model(self, request, obj, form, change):
         if not change:
             obj.author = request.user
         obj.save()
+
+    # Limit the post's list to his owner, unless he is a superuser 
+    def queryset(self, request):
+        if request.user.is_superuser:
+            return Post.objects.all()
+        return Post.objects.filter(author=request.user)
+
+    # Just to check permissions
+    def has_change_permission(self, request, obj=None):
+        has_class_permission = super(PostAdmin, self).has_change_permission(request, obj)
+        if not has_class_permission:
+            return False
+        if obj is not None and not request.user.is_superuser and request.user.id != obj.author.id:
+            return False
+        return True
 
 admin.site.register(Post,PostAdmin)
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,1 +1,23 @@
-# Create your views here.
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response
+from aaacec.apps.blog.models import Post
+
+def main(request):
+    posts = Post.objects.all().order_by("-pub_date")
+    paginator = Paginator(posts, 2)
+    
+    try: 
+        page = int(request.GET.get("page", '1'))
+    except ValueError: 
+        page = 1
+
+    try:
+        posts = paginator.page(page)
+    except (InvalidPage, EmptyPage):
+        posts = paginator.page(paginator.num_pages)
+
+    return render_to_response("post_archive.html", dict(posts=posts, user=request.user))
+
+
+
